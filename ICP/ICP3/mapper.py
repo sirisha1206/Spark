0ab1fb1e4c662d1c80ed49ab8bd0d56a3d05cbda
sirisha1206 <nagasirisha1206@gmail.com>
2018-06-18 11:12:05 -0500

    icp3

---
+++ ICP/ICP3/mapper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+row_a, col_b = 2,2
+for line in sys.stdin:
+	matrix_index, row, col, value = line.rstrip().split(",")
+	if matrix_index == 'A':
+		for i in range(0,col_b):
+			key = row + "," + str(i)
+			print("%s\t%s\t%s" %(key,col,value))
+	else:
+		for j in range(0,row_a):
+			key = str(j) + "," + col
+			print("%s\t%s\t%s" %(key,row,value))
